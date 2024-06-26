52e446a2801c1f9f144d903d428a62f1aafbc2ba
LeahyJK <leahyjared2@gmail.com>
2024-06-26 11:58:34 -0400

    modified

---
+++ index.js
@@ -0,0 +1,11 @@
+let resumeButton = document.querySelector("button");
+
+resumeButton.addEventListener("click", function(){
+    window.open("https://docs.google.com/document/d/1TdFoiaaqPmsX7kZdH7mlBWvy-wlhGo3lfOWgIVXQ6xk/edit#heading=h.xngoztqg2w5j", "_blank");
+});
+resumeButton.addEventListener("mouseover", function(){
+    resumeButton.style.backgroundColor = 'rgb(237, 191, 76)';
+    resumeButton.addEventListener("mouseout", function(){
+        resumeButton.style.backgroundColor = 'goldenrod'
+    });
+});
