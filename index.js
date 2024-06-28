2be3824bdc5a913ddbd4cf93a888d80fbe09a07c
LeahyJK <leahyjared2@gmail.com>
2024-06-28 13:03:11 -0400

    modified backgrounds and sections

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let resumeButton = document.querySelector("button");
+let sect = document.querySelector(".section")
+let homeSect = document.querySelector(".home-sect")
 
 resumeButton.addEventListener("click", function(){
     window.open("https://docs.google.com/document/d/1TdFoiaaqPmsX7kZdH7mlBWvy-wlhGo3lfOWgIVXQ6xk/edit#heading=h.xngoztqg2w5j", "_blank");
@@ -9,3 +11,4 @@ resumeButton.addEventListener("mouseover", function(){
         resumeButton.style.backgroundColor = 'goldenrod'
     });
 });
+
