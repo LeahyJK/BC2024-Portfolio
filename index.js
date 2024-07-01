801370a8bacfa8d58dc4cf26410cfa24a0276ced
LeahyJK <leahyjared2@gmail.com>
2024-07-01 15:40:14 -0400

    modified

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let resumeButton = document.querySelector("button");
-let sect = document.querySelector(".section")
-let homeSect = document.querySelector(".home-sect")
 
 resumeButton.addEventListener("click", function(){
-    window.open("https://docs.google.com/document/d/1TdFoiaaqPmsX7kZdH7mlBWvy-wlhGo3lfOWgIVXQ6xk/edit#heading=h.xngoztqg2w5j", "_blank");
+    // window.open("https://docs.google.com/document/d/1TdFoiaaqPmsX7kZdH7mlBWvy-wlhGo3lfOWgIVXQ6xk/edit#heading=h.xngoztqg2w5j", "_blank");
 });
 resumeButton.addEventListener("mouseover", function(){
     resumeButton.style.backgroundColor = 'rgb(237, 191, 76)';
+    resumeButton.style.cursor = 'pointer'; 
+
     resumeButton.addEventListener("mouseout", function(){
         resumeButton.style.backgroundColor = 'goldenrod'
     });
